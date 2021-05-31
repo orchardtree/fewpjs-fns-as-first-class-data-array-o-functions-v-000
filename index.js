07829e7c7575b11f37ed540931e96750ded9ca1a
Jesse Wenger <wengerj@gmail.com>
2021-05-31 20:00:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function wakeDog(dogName, dogBreed) {
   const wakeString = `Wake ${dogName} the ${dogBreed}`;
   console.log(wakeString);
-  return wakeString
+  return wakeString;
+}
+
+function leashDog(dogName, dogBreed) {
+  const leashString = `Leash ${dogName} the ${dogBreed}`;
+  console.log(leashString);
+  return leashString;
 }
